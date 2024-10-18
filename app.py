66176be7d914a8dd2c5650f9dfa39c90d9302f00
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-10-18 23:52:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request
 import google.generativeai as genai
 import textblob
 import os
@@ -8,7 +8,6 @@ genai.configure(api_key=api)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
-app.secret_key = 'AIzaSyBazSelM-6qpQsayREyC53xEBQc3fTpffc'  # Add a secret key for message flashing
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -22,21 +21,16 @@ def financial_FAQ():
 def makersuite():
     if request.method == "POST":
         q = request.form.get("q")
-        if not q:
-            flash('No question provided!', 'error')
-            return redirect(url_for('makersuite'))
         try:
             r = model.generate_content(q)
             if r:
                 return render_template("makersuite.html", r=r.text)
             else:
-                flash('No response from model', 'error')
-                return redirect(url_for('makersuite'))
+                return render_template("makersuite.html", error="No response from model.")
         except Exception as e:
-            flash(f'An error occurred: {str(e)}', 'error')
-            return redirect(url_for('makersuite'))
+            return render_template("makersuite.html", error=f"An error occurred: {str(e)}")
     else:
-        return render_template("makersuite.html", r=None)
+        return render_template("makersuite.html")
 
 @app.route("/sentiment_analysis", methods=["GET", "POST"])
 def sentiment_analysis():
