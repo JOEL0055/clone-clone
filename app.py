8ed11bc12d25cb2e8a5322fd33842f8dea5cbf97
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-10-18 15:17:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,41 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask,render_template,request
+import google.generativeai as genai
+import textblob
+import os
 
-app = Flask(__name__, template_folder="/Users/joelaiweithai/Downloads/BC3411/templates")
+api = os.getenv("MAKERSUITE")
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-api_key = "AIzaSyD8ocNijPFuNfaae5VDChD9jG2cXd12G50"
-model_id = "gemini-1.5-flash-latest"
-url = f"https://generativelanguage.googleapis.com/v1beta/models/{model_id}:generateContent?key={api_key}"
+app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
-@app.route("/financial_FAQ", methods=["GET", "POST"])
+@app.route("/financial_FAQ",methods=["GET","POST"])
 def financial_FAQ():
-    return render_template("financial_FAQ.html")
+    return(render_template("financial_FAQ.html"))
 
-@app.route("/makersuite", methods=["GET", "POST"])
+@app.route("/makersuite",methods=["GET","POST"])
 def makersuite():
     q = request.form.get("q")
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "contents": [
-            {
-                "parts": [
-                    {"text": q}
-                ]
-            }
-        ]
-    }
-    try:
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        result = response.json()['candidates'][0]['content']['parts'][0]['text']
-    except Exception as e:
-        result = f"An error occurred: {str(e)}"
-    
-    return render_template("makersuite.html", r=result)
+    r = model.generate_content(q)
+    return(render_template("makersuite.html",r=r.text))
+
+@app.route("/sentiment_analysis",methods=["GET","POST"])
+def sentiment_analysis():
+    return(render_template("sentiment_analysis.html"))
+
+@app.route("/transfer_money",methods=["GET","POST"])
+def transfer_money():
+    return(render_template("transfer_money.html"))
+
+@app.route("/sentiment_analysis_result",methods=["GET","POST"])
+def sentiment_analysis_result():
+    q = request.form.get("q")
+    r=textblob.TextBlob(q).sentiment
+    return(render_template("sentiment_analysis_result.html",r=r))
 
 if __name__ == "__main__":
     app.run()
